c88d17f373ef5751dd3793c0d4e885b14e30d117
LinZhou <1207032539@qq.com>
2023-08-30 13:56:35 +0800

    设置路由

--- main.go
+++ main.go
@@ -2,21 +2,14 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"go_test/controller/common_controller"
-	"go_test/helper/snowflake"
+	"go_test/route"
 )
 
 func main() {
 	gin.SetMode(gin.ReleaseMode)
-	r := gin.Default()
-	var workers [1024]*snowflake.Worker
-	for i := 0; i < 1024; i++ {
-		w, _ := snowflake.NewWorker(int64(i))
-		workers[i] = w
-	}
+	engine := gin.Default()
 
-	r.GET("/create", func(c *gin.Context) {
-		common_controller.CreateSnowflakeId(c, workers)
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080
+	route.InitRouter(engine)
+
+	engine.Run() // listen and serve on 0.0.0.0:8080
 }
---
+++ route/route.go
@@ -0,0 +1,18 @@
+package route
+
+import (
+	"github.com/gin-gonic/gin"
+	"go_test/controller/common_controller"
+	"go_test/helper/snowflake"
+)
+
+func InitRouter(e *gin.Engine) {
+	var workers [1024]*snowflake.Worker
+	for i := 0; i < 1024; i++ {
+		w, _ := snowflake.NewWorker(int64(i))
+		workers[i] = w
+	}
+	e.Any("/snowflake", func(c *gin.Context) {
+		common_controller.CreateSnowflakeId(c, workers)
+	})
+}
