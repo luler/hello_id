31ee812946eac0217d0063d9da24c668b29eba7f
LinZhou <1207032539@qq.com>
2023-09-27 14:13:17 +0800

    验证器优化

--- app/helper/valid_helper/valid_helper.go
+++ app/helper/valid_helper/valid_helper.go
@@ -7,20 +7,30 @@ import (
 	zh_translations "github.com/go-playground/validator/v10/translations/zh"
 	"go_test/app/helper/exception_helper"
 	"reflect"
+	"sync"
 )
+
+var uni *ut.UniversalTranslator
+var trans ut.Translator
+var validate *validator.Validate
+var once sync.Once
 
 // 校验参数
 // 参考使用：https://pkg.go.dev/github.com/go-playground/validator/v10
 func Check(data interface{}) {
-	uni := ut.New(zh.New())
-	trans, _ := uni.GetTranslator("zh")
-	validate := validator.New()
-	//注册一个函数，获取struct tag里自定义的label作为字段名
-	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
-		name := fld.Tag.Get("label")
-		return name
+	once.Do(func() {
+		uni = ut.New(zh.New())
+		trans, _ = uni.GetTranslator("zh")
+
+		validate = validator.New()
+		//注册一个函数，获取struct tag里自定义的label作为字段名
+		validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+			name := fld.Tag.Get("label")
+			return name
+		})
+		zh_translations.RegisterDefaultTranslations(validate, trans)
 	})
-	zh_translations.RegisterDefaultTranslations(validate, trans)
+
 	err := validate.Struct(data)
 	if err != nil {
 		for _, err := range err.(validator.ValidationErrors) {
