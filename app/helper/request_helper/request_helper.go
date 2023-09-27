6855f2b8667b062739922faeaf3261fb4d4fd559
LinZhou <1207032539@qq.com>
2023-09-27 14:50:38 +0800

    获取请求参数优化

--- app/helper/request_helper/request_helper.go
+++ app/helper/request_helper/request_helper.go
@@ -30,60 +30,22 @@ func Input(c *gin.Context, fields ...string) map[string]interface{} {
 
 // 获取GET请求参数
 func ParamGet(c *gin.Context, fields ...string) map[string]interface{} {
-	param := make(map[string]interface{})
-	// 获取所有的 GET 参数
-	queryParams := c.Request.URL.Query()
-	for key, value := range extractParam(c, queryParams, "get") {
-		param[key] = value
-	}
-	//参数过滤
-	param = helper.FilterMap(param, fields)
-	return param
+	return paramAll(c, "ParamGet", fields...)
 }
 
 // 获取PostForm请求参数
 func ParamPostForm(c *gin.Context, fields ...string) map[string]interface{} {
-	param := make(map[string]interface{})
-	// 获取所有的PostForm参数
-	c.Request.ParseForm()
-	queryParams := c.Request.PostForm
-	for key, value := range extractParam(c, queryParams, "post") {
-		param[key] = value
-	}
-
-	return param
+	return paramAll(c, "ParamPostForm", fields...)
 }
 
 // 获取MultipartForm请求参数
 func ParamMultipartForm(c *gin.Context, fields ...string) map[string]interface{} {
-	param := make(map[string]interface{})
-	// 获取multi-form类型所有的 POST 参数
-	err := c.Request.ParseMultipartForm(32 << 24)
-	if err == nil {
-		queryParams := c.Request.MultipartForm.Value
-		for key, value := range extractParam(c, queryParams, "post") {
-			param[key] = value
-		}
-	}
-	//参数过滤
-	param = helper.FilterMap(param, fields)
-	return param
+	return paramAll(c, "ParamMultipartForm", fields...)
 }
 
 // 获取json格式的请求参数
 func ParamRawJson(c *gin.Context, fields ...string) map[string]interface{} {
-	param := make(map[string]interface{})
-	//获取json类型参数
-	raw, _ := c.GetRawData()
-	jsonData := make(map[string]interface{})
-	json.Unmarshal(raw, &jsonData)
-	for key, value := range jsonData {
-		param[key] = value
-	}
-
-	//参数过滤
-	param = helper.FilterMap(param, fields)
-	return param
+	return paramAll(c, "ParamRawJson", fields...)
 }
 
 // 获取参数并验证
@@ -156,5 +118,52 @@ func extractParam(c *gin.Context, queryParams url.Values, t string) map[string]i
 		}
 	}
 
+	return param
+}
+
+// 获取请求参数
+func paramAll(c *gin.Context, method string, fields ...string) map[string]interface{} {
+	data, exists := c.Get(method)
+	param := make(map[string]interface{})
+	if !exists {
+		switch method {
+		case "ParamGet":
+			// 获取所有的 GET 参数
+			queryParams := c.Request.URL.Query()
+			for key, value := range extractParam(c, queryParams, "get") {
+				param[key] = value
+			}
+		case "ParamPostForm":
+			// 获取所有的PostForm参数
+			c.Request.ParseForm()
+			queryParams := c.Request.PostForm
+			for key, value := range extractParam(c, queryParams, "post") {
+				param[key] = value
+			}
+		case "ParamMultipartForm":
+			// 获取multi-form类型所有的 POST 参数
+			err := c.Request.ParseMultipartForm(32 << 24)
+			if err == nil {
+				queryParams := c.Request.MultipartForm.Value
+				for key, value := range extractParam(c, queryParams, "post") {
+					param[key] = value
+				}
+			}
+		case "ParamRawJson":
+			//获取json类型参数
+			raw, _ := c.GetRawData()
+			jsonData := make(map[string]interface{})
+			json.Unmarshal(raw, &jsonData)
+			for key, value := range jsonData {
+				param[key] = value
+			}
+		}
+		c.Set(method, param)
+	} else {
+		param = data.(map[string]interface{})
+	}
+
+	//参数过滤
+	param = helper.FilterMap(param, fields)
 	return param
 }
