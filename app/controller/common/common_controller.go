0a24b5f00ab17ca1e3735a71818f6a5e6da2b12a
LinZhou <1207032539@qq.com>
2023-10-10 17:53:45 +0800

    获取ID数量控制

--- app/controller/common/common_controller.go
+++ app/controller/common/common_controller.go
@@ -7,69 +7,127 @@ import (
 	uuid "github.com/satori/go.uuid"
 	"github.com/segmentio/ksuid"
 	"github.com/sony/sonyflake"
+	"go_test/app/helper/exception_helper"
 	"go_test/app/helper/response_helper"
 	"strconv"
 )
 
 // Snowflake 创建雪花ID
 func Snowflake(c *gin.Context) {
-	worker_id, _ := strconv.Atoi(c.Query("worker_id"))
-	if worker_id < 0 || worker_id > 1023 {
+	workerId, _ := strconv.Atoi(c.Query("workerId"))
+	length, _ := strconv.Atoi(c.Query("length"))
+	if workerId < 0 || workerId > 1023 {
 		response_helper.Fail(c, "机器ID只能设置在0-1023之间")
 		return
 	}
 
-	node, _ := snowflake.NewNode(int64(worker_id))
+	node, _ := snowflake.NewNode(int64(workerId))
+	if length <= 0 {
+		length = 1
+	} else if length > 500 {
+		exception_helper.CommonException("单次生成数量不能大于500")
+	}
+	var ids []string
+	for i := 0; i < length; i++ {
+		ids = append(ids, node.Generate().String())
+	}
 
 	response_helper.Success(c, "创建成功", gin.H{
-		"type":      "snowflake",
-		"worker_id": worker_id,
-		"id":        node.Generate(),
+		"type":     "snowflake",
+		"workerId": workerId,
+		"ids":      ids,
 	})
 }
 
 // Sonyflake 创建sonyflake ID
 func Sonyflake(c *gin.Context) {
+	length, _ := strconv.Atoi(c.Query("length"))
+	if length <= 0 {
+		length = 1
+	} else if length > 500 {
+		exception_helper.CommonException("单次生成数量不能大于500")
+	}
 	sf, _ := sonyflake.New(sonyflake.Settings{})
-	id, _ := sf.NextID()
+	var ids []string
+	var id uint64
+	for i := 0; i < length; i++ {
+		id, _ = sf.NextID()
+		ids = append(ids, strconv.FormatUint(id, 10))
+	}
 	response_helper.Success(c, "创建成功", gin.H{
 		"type": "sonyflake",
-		"id":   strconv.FormatUint(id, 10),
+		"ids":  ids,
 	})
 }
 
 // Uuid1 生成uuid版本1类型的id
 func Uuid1(c *gin.Context) {
-	uuid1 := uuid.NewV1()
+	length, _ := strconv.Atoi(c.Query("length"))
+	if length <= 0 {
+		length = 1
+	} else if length > 500 {
+		exception_helper.CommonException("单次生成数量不能大于500")
+	}
+	var ids []string
+	for i := 0; i < length; i++ {
+		ids = append(ids, uuid.NewV1().String())
+	}
 	response_helper.Success(c, "创建成功", gin.H{
 		"type": "uuid1",
-		"id":   uuid1.String(),
+		"ids":  ids,
 	})
 }
 
 // Uuid4 生成uuid版本4类型的id
 func Uuid4(c *gin.Context) {
-	uuid4 := uuid.NewV4()
+	length, _ := strconv.Atoi(c.Query("length"))
+	if length <= 0 {
+		length = 1
+	} else if length > 500 {
+		exception_helper.CommonException("单次生成数量不能大于500")
+	}
+	var ids []string
+	for i := 0; i < length; i++ {
+		ids = append(ids, uuid.NewV4().String())
+	}
 	response_helper.Success(c, "创建成功", gin.H{
 		"type": "uuid4",
-		"id":   uuid4.String(),
+		"ids":  ids,
 	})
 }
 
 // Xid 生成Xid类型的id
 func Xid(c *gin.Context) {
-	guid := xid.New()
+	length, _ := strconv.Atoi(c.Query("length"))
+	if length <= 0 {
+		length = 1
+	} else if length > 500 {
+		exception_helper.CommonException("单次生成数量不能大于500")
+	}
+	var ids []string
+	for i := 0; i < length; i++ {
+		ids = append(ids, xid.New().String())
+	}
 	response_helper.Success(c, "创建成功", gin.H{
 		"type": "xid",
-		"id":   guid.String(),
+		"ids":  ids,
 	})
 }
 
 // Ksuid 生成Ksuid类型的id
 func Ksuid(c *gin.Context) {
-	guid := ksuid.New()
+	length, _ := strconv.Atoi(c.Query("length"))
+	if length <= 0 {
+		length = 1
+	} else if length > 500 {
+		exception_helper.CommonException("单次生成数量不能大于500")
+	}
+	var ids []string
+	for i := 0; i < length; i++ {
+		ids = append(ids, ksuid.New().String())
+	}
 	response_helper.Success(c, "创建成功", gin.H{
 		"type": "ksuid",
-		"id":   guid.String(),
+		"ids":  ids,
 	})
 }
